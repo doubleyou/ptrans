049ffe315e76c4be7e5a0283974881e0009bde40
Dmitry Demeshchuk <demeshchuk@gmail.com>
2011-11-04 21:25:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+ptrans - a bunch of handy parse transforms
+==========================================
+
+1. pl2rec - proplist to record converter
+========================================
+
+A lot of people prefer to pass gen_server options as a proplist and store them in a state being a record. So, they generally write something like that:
+
+```erlang
+
+-record(state, {
+    port = 8080,
+    ip = {127,0,0,1},
+    name
+}).
+
+...
+
+init(Options) ->
+    State = parse_options(Options, State),
+    {ok, State}.
+
+...
+
+parse_options([], State) -> State;
+parse_options([{port, Port} | T], State) ->
+    parse_options(T, State#state{port = Port});
+parse_options([{ip, IP} | T], State) ->
+    parse_options(T, State#state{ip = IP});
+parse_options([{name, Name} | T], State) ->
+    parse_options(T, State#state{name = Name}).
+
+```
+
+Annoying, isn't it?
+
+But instead, you could just write:
+
+```erlang
+-compile({parse_transform, pl2rec}).
+
+-record(state, {
+    port = 8080,
+    ip = {127,0,0,1},
+    name
+}).
+
+init(Options) ->
+    State = parse_options(Options, State),
+    {ok, State}.
+```
+
+You can even parse multiple records this way. They may have same field names, still the parsing mechanism will work properly.
+
+If you have some unexisting properties in your proplists, they will be just silently ignored during the parsing process.
+
+Basic type-check guards will be added soon (they'll check if a given record has type definitions and insert the proper guards to the generated parse_options function.
---
+++ src/pl2rec.erl
@@ -0,0 +1,85 @@
+-module(pl2rec).
+-export([parse_transform/2]).
+
+parse_transform(Forms, _Options) ->
+    Records = parse_records(Forms),
+%%    error_logger:error_report(Forms),
+    {eof, Line} = lists:keyfind(eof, 1, Forms),
+    ParseFunc = parsefunc_body(Records, Line),
+    NewForms = lists:takewhile(
+        fun ({eof, _}) -> false; (_) -> true end,
+        Forms) ++ [ParseFunc] ++ [{eof, Line + parsefunc_lines(Records) + 1}],
+    NewForms.
+
+parse_records(Forms) ->
+    RawRecords = lists:filter(
+        fun ({attribute, _, record, _}) -> true; (_) -> false end,
+        Forms
+    ),
+    [{RecName, parse_record_fields(RecRawFields)}
+        || {attribute, _, record, {RecName, RecRawFields}} <- RawRecords].
+
+parse_record_fields(RawFields) ->
+    lists:map(
+        fun ({record_field, _, {_, _, Field}}) -> Field;
+            ({record_field, _, {_, _, Field}, _}) -> Field
+        end,
+        RawFields
+    ).
+
+parsefunc_body(Records, Line) ->
+    {Clauses, LastLine} = lists:foldl(
+        fun ({Record, Fields}, {Cs, L}) ->
+            {
+                parsefunc_record_clauses(Record, Fields, L) ++ Cs,
+                L+2*length(Fields)
+            }
+        end,
+        {[], Line+1},
+        Records
+    ),
+    {function,Line,parse_options,2,
+        [parsefunc_tail(Line) | lists:reverse(Clauses)] ++
+            [parsefunc_skip(LastLine + 1)]
+    }.
+
+parsefunc_tail(Line) ->
+    {clause,Line,[{nil,Line},{var,Line,'R'}],[],[{var,Line,'R'}]}.
+
+parsefunc_record_clauses(Record, Fields, Line) ->
+    {Clauses, _} = lists:foldl(
+        fun(Field, {Cs, L}) ->
+            {[parsefunc_clause(Record, Field, L)| Cs], L+2}
+        end,
+        {[], Line},
+        Fields
+    ),
+    lists:reverse(Clauses).
+
+parsefunc_clause(Record, Field, Line) ->
+    {clause,Line,
+             [{cons,Line,
+                  {tuple,Line,[{atom,10,Field},{var,10,'V'}]},
+                  {var,Line,'T'}},
+              {match,Line,{var,Line,'R'},{record,Line,Record,[]}}],
+             [],
+             [{call,Line+1,
+                  {atom,Line+1,parse_options},
+                  [{var,Line+1,'T'},
+                   {record,Line+1,
+                       {var,Line+1,'R'},
+                       Record,
+                       [{record_field,Line+1,
+                            {atom,Line+1,Field},
+                            {var,Line+1,'V'}}]}]}]}.
+
+parsefunc_skip(Line) ->
+    {clause,Line,
+               [{cons,Line,{var,Line,'_'},{var,Line,'T'}},{var,Line,'R'}],
+               [],
+               [{call,Line+1,
+                      {atom,Line+1,parse_options},
+                      [{var,Line+1,'T'},{var,Line+1,'R'}]}]}.
+
+parsefunc_lines(Records) ->
+    lists:sum([length(Fields) || {_Rec, Fields} <- Records]) + 3.
